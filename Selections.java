82729c598170225a2f1f7a2b16b44a616eb71ffc
QuantumBlink1337 <thequantumblink@gmail.com>
2021-05-31 22:17:47 -0500

    changed Exception name

--- Selections.java
+++ Selections.java
@@ -2,7 +2,40 @@
     This enumerator class defines three Selections objects that a Player can have.
 */ 
 public enum Selections {
-        ROCK,
-        PAPER,
-        SCISSORS;
-    }
+    ROCK,
+    PAPER,
+    SCISSORS,
+    // public int compareWins(Selections selection) {
+    //     switch (this) {
+    //         case ROCK:
+    //             switch (selection) {
+    //                 case ROCK:
+    //                     return 0;
+    //                 case PAPER:
+    //                     return -1;
+    //                 case SCISSORS:
+    //                     return 1;
+    //            }
+    //         case PAPER:
+    //             switch (selection) {
+    //                 case ROCK:
+    //                     return -1;
+    //                 case PAPER:
+    //                     return 0;
+    //                 case SCISSORS:
+    //                     return 1;
+    //            }
+    //         case SCISSORS:
+    //            switch (selection) {
+    //                case ROCK:
+    //                     return 1;
+    //                 case PAPER:
+    //                     return -1;
+    //                 case SCISSORS:
+    //                     return 0;
+    //            }
+    //     }
+    // }
+}
+
+
